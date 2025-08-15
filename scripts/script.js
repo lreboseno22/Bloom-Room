30c0f2e18a6c49b45e7be8cdf252534bd6854ca5
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-08-15 12:46:11 -0400

    create garden and cells

--- scripts/script.js
+++ scripts/script.js
@@ -17,5 +17,22 @@ const playerNameEl = document.getElementById("playerName");
 if (playerNameEl) {
     const name = localStorage.getItem("playerName");
     playerNameEl.textContent = name || "gardener";
+
+    // Dynamically create garden 
+    const gardenGrid = document.getElementById("gardenGrid");
+    const rows = 10;
+    const cols = 10;
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.dataset.row = r;
+            cell.dataset.col = c;
+            
+            gardenGrid.appendChild(cell);
+        }
+    }
+
 }
 
